97492f06e977810e8b500da8ad9c66e673cd0513
joohee-kim <joohee.kim15@navercorp.com>
2020-09-14 20:41:43 +0900

    :construction: 리졸버 추가

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -1,14 +1,16 @@
 const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schema');
 const {createStore} = require('./utils');
+const resolvers = require('./resolvers');
 
-const LaunchAPI = require('./databases/launch');
-const UserAPI = require('./databases/user');
+const LaunchAPI = require('./datasources/launch');
+const UserAPI = require('./datasources/user');
 
 const store = createStore();
 
 const server = new ApolloServer({ 
     typeDefs,
+    resolvers,
     dataSources: () => ({
         launchAPI: new LaunchAPI(),
         userAPI: new UserAPI({store})
