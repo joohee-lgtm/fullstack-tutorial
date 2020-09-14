cd66dd26d2a46720b6ccee2b936bfc0e2c0a4fd1
joohee-kim <joohee.kim15@navercorp.com>
2020-09-14 20:57:59 +0900

    :construction: 다른 리졸버 추가

--- start/server/src/resolvers.js
+++ start/server/src/resolvers.js
@@ -5,6 +5,25 @@ module.exports = {
         launch: (_, {id}, {dataSources}) => dataSources.launchAPI.getLaunchById({launchId: id}),
         me: (_, __, {dataSources}) => dataSources.userAPI.findOrCreateUser(),
         LaunchIds: (_, __, {dataSources}) => dataSources.userAPI.getLaunchIdsByUser()
+    },
+    Mission: {
+        missionPatch: (mission, {size} = {size: 'LARGE'}) => {
+            return size === 'SMALL'
+                ? mission.missionPatchSmall
+                : mission.missionPatchLarge;
+        }
+    },
+    User: {
+        trips: async (_, __, {dataSources}) => {
+            const launchIds = await dataSources.userAPI.getLaunchIdsByUser();
+
+            if(!launchIds.length) {
+                return [];
+            }
+            return dataSources.launchAPI.getLaunchById({
+                launchIds,
+            }) || []
+        }
     }
 }
 
