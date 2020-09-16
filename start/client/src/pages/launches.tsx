93da4fd414a3dea397c329e76dae4057209bab02
USER <joohee.kim15@navercorp.com>
2020-09-16 10:21:51 +0900

    launch 페이지에서 사용할 gql 작성

--- start/client/src/pages/launches.tsx
+++ start/client/src/pages/launches.tsx
@@ -1,6 +1,30 @@
 import React, { Fragment }  from 'react';
 import { RouteComponentProps } from '@reach/router';
-import { gql } from '@apollo/client'
+import { gql, useQuery, from } from '@apollo/client'
+
+import {LaunchTile, Header, Button, Loading} from '../components';
+import * as GetLaunchListTypes from './__generated__/GetLaunchList';
+
+const GET_LAUNCHES = gql`
+  query launchList($after: String) {
+    launches(after: $after) {
+      cursor
+      hasMore
+      launches {
+        id
+        isBooked
+        rocket {
+          id
+          name
+        }
+        mission {
+          name
+          missionPatch
+        }
+      }
+    }
+  }
+`;
 
 export const LAUNCH_TILE_DATA = gql`
   fragment LaunchTile on Launch {
