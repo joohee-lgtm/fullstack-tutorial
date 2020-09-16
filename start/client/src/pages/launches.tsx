450b0be6517c1022d676a24de6a600f94cfc8e05
USER <joohee.kim15@navercorp.com>
2020-09-16 10:44:15 +0900

    launch 호출 / 렌더링 부 추가

--- start/client/src/pages/launches.tsx
+++ start/client/src/pages/launches.tsx
@@ -1,9 +1,10 @@
-import React, { Fragment }  from 'react';
-import { RouteComponentProps } from '@reach/router';
-import { gql, useQuery, from } from '@apollo/client'
+import React, { Fragment } from "react";
+import { RouteComponentProps } from "@reach/router";
+import { gql, useQuery, from } from "@apollo/client";
 
-import {LaunchTile, Header, Button, Loading} from '../components';
-import * as GetLaunchListTypes from './__generated__/GetLaunchList';
+import { LaunchTile, Header, Button, Loading } from "../components";
+import * as GetLaunchListTypes from "./__generated__/GetLaunchList";
+import { GetLaunch } from "../containers/__generated__/GetLaunch";
 
 const GET_LAUNCHES = gql`
   query launchList($after: String) {
@@ -45,7 +46,33 @@ export const LAUNCH_TILE_DATA = gql`
 interface LaunchesProps extends RouteComponentProps {}
 
 const Launches: React.FC<LaunchesProps> = () => {
-  return <div />;
-}
+  const { data, loading, error } = useQuery<
+    GetLaunchListTypes.GetLaunchList,
+    GetLaunchListTypes.GetLaunchListVariables
+  >(GET_LAUNCHES);
+
+  if (loading) {
+    return <Loading />;
+  }
+
+  if (error) {
+    return <p> Error </p>;
+  }
+
+  if (!data) {
+    return <p>Not Found</p>;
+  }
+
+  return (
+    <Fragment>
+      <Header />
+      {data.launches &&
+        data.launches.launches &&
+        data.launches.launches.map((launch: any) => (
+          <LaunchTile key={launch.id} launch={launch} />
+        ))}
+    </Fragment>
+  );
+};
 
 export default Launches;
