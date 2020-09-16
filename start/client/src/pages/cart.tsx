28f3ae47666cfd882a0f279893235e357761cad4
USER <joohee.kim15@navercorp.com>
2020-09-16 12:26:03 +0900

    isInCart 판단가능한 리졸버 추가

--- start/client/src/pages/cart.tsx
+++ start/client/src/pages/cart.tsx
@@ -1,12 +1,48 @@
-import React from 'react';
-import { RouteComponentProps } from '@reach/router';
+import React, { Fragment } from "react";
+import { gql, useQuery } from "@apollo/client";
 
-interface CartProps extends RouteComponentProps {
+import { Header, Loading } from "../components";
+import { CartItem, BookTrips } from "../containers";
+import { RouteComponentProps } from "@reach/router";
+import * as GetCartItemsTypes from "./__generated__/GetCartItems";
 
-}
+export const GET_CART_ITEMS = gql`
+  query GetCartItems {
+    cartItems @client
+  }
+`;
+
+interface CartProps extends RouteComponentProps {}
 
 const Cart: React.FC<CartProps> = () => {
-  return <div />;
-}
+  const { data, loading, error } = useQuery<GetCartItemsTypes.GetCartItems>(
+    GET_CART_ITEMS
+  );
+
+  if (loading) {
+    return <Loading></Loading>;
+  }
+
+  if (error) {
+    return <p>Error : {error.message}</p>;
+  }
+
+  return (
+    <>
+      <Header>My Cart</Header>
+      {!data || (!!data && data.cartItems.length === 0) ? (
+        <p data-testid="empty-message">No Items in your cart</p>
+      ) : (
+        <Fragment>
+          {!!data &&
+            data.cartItems.map((launchId: any) => (
+              <CartItem key={launchId} launchId={launchId} />
+            ))}
+          <BookTrips cartItems={!!data ? data.cartItems : []}></BookTrips>
+        </Fragment>
+      )}
+    </>
+  );
+};
 
 export default Cart;
--- start/client/src/resolvers.tsx
+++ start/client/src/resolvers.tsx
@@ -2,6 +2,7 @@ import { gql, ApolloCache, Resolvers } from "@apollo/client";
 
 import * as GetCartItemTypes from "./pages/__generated__/GetCartItems";
 import * as LaunchTileTypes from "./pages/__generated__/LaunchTile";
+import { GET_CART_ITEMS } from "./pages/cart";
 
 export const typeDefs = gql`
   extend type Query {
@@ -28,8 +29,30 @@ interface ResolverMap {
   [field: string]: ResolverFn;
 }
 
-interface AppResolvers extends Resolvers {}
+interface AppResolvers extends Resolvers {
+  Launch: ResolverMap;
+}
 
-export const resolvers = {};
+export const resolvers: AppResolvers = {
+  Launch: {
+    isInCart: (launch: LaunchTileTypes.LaunchTile, _, { cache }): boolean => {
+      const queryResult = cache.readQuery<GetCartItemTypes.GetCartItems>({
+        query: GET_CART_ITEMS,
+      });
+
+      if (queryResult) {
+        return queryResult.cartItems.includes(launch.id);
+      }
+
+      return false;
+    },
+  },
+};
+
+export const scheme = gql`
+  extend type Launch {
+    isInCart: Boolean!
+  }
+`;
 
 export default {};
