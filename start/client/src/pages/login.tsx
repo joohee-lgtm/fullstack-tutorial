382c26739763350740847df7b79fc2f00f16b8eb
USER <joohee.kim15@navercorp.com>
2020-09-16 11:36:09 +0900

    로그인 화면 추가

--- start/client/src/pages/login.tsx
+++ start/client/src/pages/login.tsx
@@ -1,5 +1,39 @@
-import React from 'react';
+import React from "react";
+import {
+  gql,
+  ApolloClient,
+  useApolloClient,
+  useMutation,
+} from "@apollo/client";
+
+import { LoadingForm, Loading, LoginForm } from "../components";
+import * as LoginTypes from "./__generated__/login";
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!) {
+    login(email: $email)
+  }
+`;
 
 export default function Login() {
-  return <div />;
+  const client: ApolloClient<any> = useApolloClient();
+  const [login, { loading, error }] = useMutation<
+    LoginTypes.login,
+    LoginTypes.loginVariables
+  >(LOGIN_USER, {
+    onCompleted({ login }) {
+      localStorage.setItem("token", login as string);
+      client.writeData({ data: { isLoggedIn: true } });
+    },
+  });
+
+  if (loading) {
+    return <Loading></Loading>;
+  }
+
+  if (error) {
+    return <p>An Error Occured</p>;
+  }
+
+  return <LoginForm login={login}></LoginForm>;
 }
